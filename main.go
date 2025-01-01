cb48190a4e2eef55d988afaa2a0c0ecccfa9c2a3
cooler-SAI <cooler19860212@yahoo.com>
2025-01-01 22:24:38 +0300

    added code to repo

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/IBM/sarama"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello Kafka Go")
+	brokerList := []string{"localhost:9092"}
+	config := sarama.NewConfig()
+
+	producer, err := sarama.NewSyncProducer(brokerList, config)
+	if err != nil {
+		log.Fatalf("Error creating producer: %s\n", err)
+	}
+	defer func(producer sarama.SyncProducer) {
+		err := producer.Close()
+		if err != nil {
+			log.Fatalf("Error closing producer: %s\n", err)
+		}
+	}(producer)
+
+	msg := &sarama.ProducerMessage{
+		Topic: "test",
+		Value: sarama.StringEncoder("hello Kafka from Go"),
+	}
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		log.Fatalf("Error sending message: %s\n", err)
+
+	}
+	fmt.Printf("Partition: %d, Offset: %d\n", partition, offset)
 }
