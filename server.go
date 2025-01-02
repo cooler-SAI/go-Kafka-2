2263f690ea30ceb4c5f8777917117285471e657f
cooler-SAI <cooler19860212@yahoo.com>
2025-01-02 18:14:13 +0300

    set up logs in http server

--- server.go
+++ server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 )
 
 func handler(w http.ResponseWriter, _ *http.Request) {
@@ -11,11 +14,29 @@ func handler(w http.ResponseWriter, _ *http.Request) {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("Server is listening on port 8080\n")
+
 	fmt.Println(fprintf)
 }
 
 func main() {
+	fmt.Printf("Server is listening on port 8080\n")
+
+	server := &http.Server{
+		Addr: ":8080",
+	}
+
+	go func() {
+		fmt.Println("Server is listening on port 8080")
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, err) {
+			fmt.Printf("Error starting server: %v\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	fmt.Println("Shutting down server...")
+
 	http.HandleFunc("/", handler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
