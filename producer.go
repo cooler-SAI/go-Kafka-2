70f7599f486757b497409c64f4500957372ef690
cooler-SAI <cooler19860212@yahoo.com>
2025-01-04 00:32:43 +0300

    added producer in repo

---
+++ producer.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/IBM/sarama"
+	"log"
+)
+
+func main() {
+	brokerList := []string{"localhost:9092"}
+
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+
+	producer, err := sarama.NewSyncProducer(brokerList, config)
+	if err != nil {
+		log.Fatalf("Error creating the producer: %v", err)
+	}
+	defer func(producer sarama.SyncProducer) {
+		if err := producer.Close(); err != nil {
+			log.Printf("Error closing the producer: %v", err)
+		}
+	}(producer)
+
+	msg := &sarama.ProducerMessage{
+		Topic: "test",
+		Value: sarama.StringEncoder("hello kafka from Go"),
+	}
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		log.Fatalf("Error sending message: %v", err)
+	}
+
+	fmt.Printf("Message sent to Partition: %d, Offset: %d\n", partition, offset)
+}
